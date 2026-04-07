326c1ad973ef591c3f5dcefe863a56908c7e8fbc
Mike Strn <x@pnhd.ru>
2026-04-07 20:58:52 +0300

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        support: resolve(__dirname, 'support.html'),
+        refund: resolve(__dirname, 'refund.html'),
         thanks: resolve(__dirname, 'thanks.html'),
         privacy: resolve(__dirname, 'privacy.html'),
         terms: resolve(__dirname, 'terms.html'),
